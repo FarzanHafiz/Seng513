d6f4d5c973e025a36a0c2059bf5a976c3bef4a83
Farzan Hafiz <shahfarzan.alhafiz@ucalgary.ca>
2019-02-15 15:49:19 -0700

    update

--- A2/calculator.js
+++ A2/calculator.js
@@ -1,3 +1,15 @@
 function input(num) {
     document.form.display.value = document.form.display.value + num;
 }
+
+function equal() {
+    var exp = document.form.display.value;
+    if (exp) {
+    document.form.display.value = eval(document.form.display.value);
+    }
+}
+
+function allClear() {
+    document.form.display.value = "";
+}
+
