5986219a47eccd0db92a1499ecf1fb9f75ca38f1
Farzan Hafiz <shahfarzan.alhafiz@ucalgary.ca>
2019-02-15 18:39:06 -0700

    milestoe 2

--- A2/calculator.js
+++ A2/calculator.js
@@ -1,4 +1,10 @@
+var invalid = false;
+
 function input(num) {
+    if (invalid === true) {
+        allClear();
+        invalid = false;
+    }
     var exp = document.form.display.value;
     document.form.display.value = exp + num;
 }
@@ -12,7 +18,8 @@ function equal() {
     }
     catch (e) {
         document.form.display.value = "Invalid";
-    }   
+    invalid = true;
+    }  
 }
 
 function backSpace() {
